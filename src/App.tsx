9381385c5a928cd484dc98cad5f41e379464d731
Igor Storchevoy <hellsgor@gmail.com>
2025-04-10 00:26:12 +0400

    Feat[store]: refactor state management to support multiple counters with unique IDs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import { DecrementAction, IncrementAction, store } from './store';
+import { CounterId, DecrementAction, IncrementAction, store } from './store';
 import { useEffect, useReducer } from 'react';
 
 function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <Counter counterId="first" />
+      <Counter counterId="second" />
+      <h1>Vite + React</h1>
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
+    </>
+  );
+}
+
+export function Counter({ counterId }: { counterId: CounterId }) {
   const [, forceUpdate] = useReducer((x) => x + 1, 0);
 
   useEffect(() => {
@@ -17,21 +43,15 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <p>counter: {store.getState().counter}</p>
+        <p>counter: {store.getState().counters[counterId]?.counter ?? 0}</p>
 
         <button
           onClick={() =>
-            store.dispatch({ type: 'increment' } satisfies IncrementAction)
+            store.dispatch({
+              type: 'increment',
+              payload: { counterId },
+            } satisfies IncrementAction)
           }
         >
           increment
@@ -39,18 +59,15 @@ function App() {
 
         <button
           onClick={() =>
-            store.dispatch({ type: 'decrement' } satisfies DecrementAction)
+            store.dispatch({
+              type: 'decrement',
+              payload: { counterId },
+            } satisfies DecrementAction)
           }
         >
           decrement
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
