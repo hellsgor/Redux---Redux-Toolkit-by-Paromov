21d4d260dbfeeea2d925d0e305351b5250cc187f
Igor Storchevoy <hellsgor@gmail.com>
2025-04-13 00:18:49 +0400

    Refactor[UsersList]: update SelectedUser component to use userId prop and memoize UsersListItem

--- src/UsersList.tsx
+++ src/UsersList.tsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
+import { memo, useState } from "react";
 import {
   createAppSelector,
   useAppDispatch,
   useAppSelector,
-  User,
   UserClearSelectedAction,
   UserSelectedAction,
   RootState,
+  UserId,
 } from "./store";
 
 const selectSortedUsers = createAppSelector(
@@ -25,10 +25,10 @@ const selectSortedUsers = createAppSelector(
       }),
 );
 
-const selectSelectedUser = (state: RootState) =>
-  state.users.selectedUserId
-    ? state.users.entities[state.users.selectedUserId]
-    : undefined;
+// const selectSelectedUser = (state: RootState) =>
+//   state.users.selectedUserId
+//     ? state.users.entities[state.users.selectedUserId]
+//     : undefined;
 
 export function UsersList() {
   const [sortType, setSortType] = useState<"asc" | "desc">("asc");
@@ -36,7 +36,7 @@ export function UsersList() {
   const sortedUsers = useAppSelector((state) => {
     return selectSortedUsers(state, sortType);
   });
-  const selectedUser = useAppSelector(selectSelectedUser);
+  // const selectedUser = useAppSelector(selectSelectedUser);
 
   const handleSortButtonsClick = () => {
     setSortType((prev) => (prev === "asc" ? "desc" : "asc"));
@@ -46,7 +46,7 @@ export function UsersList() {
 
   return (
     <div className="relative flex flex-col gap-y-10">
-      {selectedUser && <SelectedUser user={selectedUser} />}
+      <SelectedUser />
 
       <div className="flex w-full items-center justify-center gap-x-2">
         sort:
@@ -60,14 +60,21 @@ export function UsersList() {
 
       <div className="flex flex-col gap-y-4">
         {sortedUsers.map((user) => (
-          <UsersListItem key={user.id} user={user} />
+          <UsersListItem key={user.id} userId={user.id} />
         ))}
       </div>
     </div>
   );
 }
 
-export function UsersListItem({ user }: { user: User }) {
+const UsersListItem = memo(function UsersListItem({
+  userId,
+}: {
+  userId: UserId;
+}) {
+  console.log(`render user ${userId}`);
+  const user = useAppSelector((state) => state.users.entities[userId]);
+
   const dispatch = useAppDispatch();
   const handleClick = () => {
     dispatch({
@@ -84,24 +91,35 @@ export function UsersListItem({ user }: { user: User }) {
       </button>
     </li>
   );
-}
+});
 
-export function SelectedUser({ user }: { user: User }) {
+export const SelectedUser = memo(function SelectedUser() {
+  const selectedUser = useAppSelector((state) =>
+    state.users.selectedUserId
+      ? state.users.entities[state.users.selectedUserId]
+      : undefined,
+  );
+
+  console.log("render SelectedUser");
   const dispatch = useAppDispatch();
   const handleClick = () => {
     dispatch({ type: "userCleared" } satisfies UserClearSelectedAction);
   };
+
+  if (!selectedUser) {
+    return null;
+  }
 
   return (
     <div className="fixed top-0 right-0 flex w-3xs flex-col gap-y-4 border p-4">
       <span className="font-bold">Selected user:</span>
 
       <div className="flex flex-col gap-y-0.5">
-        <h3>name: {user.name}</h3>
-        <span>description: {user.description}</span>
+        <h3>name: {selectedUser.name}</h3>
+        <span>description: {selectedUser.description}</span>
       </div>
 
       <button onClick={handleClick}>unselect</button>
     </div>
   );
-}
+});
